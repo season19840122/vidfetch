ff360be0a317d329854f299867f5081cb6f8f243
尋找安多芬 <season19840122@gmail.com>
2026-08-20 12:22:00 +0800

    fix: 修复地址栏首页的 title

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', name: 'home', component: () => import('@/views/HomeView.vue'), meta: { title: '首页' } },
+    { path: '/', name: 'home', component: () => import('@/views/HomeView.vue') },
     { path: '/tasks', name: 'tasks', component: () => import('@/views/TasksView.vue'), meta: { title: '下载任务' } },
     { path: '/history', name: 'history', component: () => import('@/views/HistoryView.vue'), meta: { title: '下载历史' } },
     { path: '/dashboard', name: 'dashboard', component: () => import('@/views/DashboardView.vue'), meta: { title: 'Dashboard' } },
